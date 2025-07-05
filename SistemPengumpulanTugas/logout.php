449ffb0b044eb07c543be902e6e87353cce272f6
nacho0ol <nacholivia13@gmail.com>
2025-07-05 20:45:38 +0700

    Update logout.php

--- SistemPengumpulanTugas/logout.php
+++ SistemPengumpulanTugas/logout.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
 
-// Hapus semua variabel session
 $_SESSION = array();
 
-// Hancurkan session
 session_destroy();
 
-// Redirect ke halaman login di direktori yang sama
 header("Location: login.php");
 exit;
 ?>
